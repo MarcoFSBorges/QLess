b459ad3c94df763a00eb05cac433d20b54400e0d
Marco Borges <marcofsborges@gmail.com>
2023-05-07 13:49:32 +0000

    New Dockerfile

--- deploy/webapi/scripts/clean_build_webapi.bat
+++
@@ -1,4 +0,0 @@
-cd "..\..\..\backend\jvm\webapi"
-
-rem Clean build the Spring project
-gradlew clean build
--- deploy/webapi/scripts/run_webapi.bat
+++
@@ -1,4 +0,0 @@
-cd "..\..\..\backend\jvm\webapi"
-
-rem Run the built Spring project
-java -jar build/libs/webapi.jar
