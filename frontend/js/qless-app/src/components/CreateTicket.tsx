ab6e3889fc0c59d15822f4ffdb6100662b9ec6ce
a46109 <cravojmls@gmail.com>
2023-05-30 18:37:59 +0100

    skeleton for settings screen

--- frontend/js/qless-app/src/components/CreateTicket.tsx
+++ frontend/js/qless-app/src/components/CreateTicket.tsx
@@ -1,6 +1,7 @@
 
-import { useState } from 'react';
-import { Stack, Paper, Card, Menu, MenuItem, Typography, TextField} from '@mui/material'
+import { ChangeEvent, useState } from 'react';
+import { Stack, MenuItem, Typography, TextField, IconButton, Button} from '@mui/material'
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 const categories = [
     {value: 'CD', label: 'Carimbar documento'},
@@ -14,45 +15,82 @@ const categories = [
 export function CreateTicket() {
 
     const [category, setCategory] = useState('');
-    console.log( {category} )
+    const [comment, setComment] = useState('');
+    const [file, setFile] = useState<File>();
+
+    console.log(file)
+
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCategory(event.target.value);
-        
+        setCategory(event.target.value);      
     };
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+    }
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+          setFile(e.target.files[0]);
+        }
+      };
+
 
     return(
         <>
-        <Stack direction='row' justifyContent='normal' spacing={2} paddingTop='150px' paddingLeft='250px'>
+            <Stack direction='row' justifyContent='normal' spacing={2} paddingTop='150px' paddingLeft='250px'>
                 <Typography variant='body1' fontFamily='monospace' fontSize={15}>Ticket Category</Typography>
             </Stack>
-            <Stack direction='column' justifyContent='normal'  paddingTop='10px' paddingLeft='250px'>
-                <TextField
-                    label="Category"
-                    select
-                    value={category}
-                    onChange={handleChange}
-                    fullWidth
-                    sx={{backgroundColor:'white'}}
-                > 
-                <MenuItem value='CD'>Carimbar documento</MenuItem> 
-                <MenuItem value='MT'>Mudança de turma</MenuItem> 
-                <MenuItem value='CDS'>Candidaturas</MenuItem> 
-                <MenuItem value='PP'>Pagamento de propinas</MenuItem> 
-                <MenuItem value='PPP'>Pedido de plano de pagamento</MenuItem> 
-                <MenuItem value='OT'>Outro</MenuItem> 
-                </TextField>
-                <p/>
-                <Typography align='left' variant='body1' fontFamily='monospace' fontSize={15} paddingBottom={1}>Add comment</Typography>
-                <TextField 
-                    label="Comment"
-                    fullWidth
-                    multiline
-                    rows={8}
-                    sx={{backgroundColor:'white'}}>
+            <form onSubmit={handleSubmit}>
+                <Stack direction='column' justifyContent='normal'  paddingTop='10px' paddingLeft='250px'>
+                    <TextField
+                        label="Category"
+                        select
+                        value={category}
+                        onChange={handleChange}
+                        fullWidth
+                        sx={{backgroundColor:'white'}}
+                    > 
+                    <MenuItem value='CD'>Carimbar documento</MenuItem> 
+                    <MenuItem value='MT'>Mudança de turma</MenuItem> 
+                    <MenuItem value='CDS'>Candidaturas</MenuItem> 
+                    <MenuItem value='PP'>Pagamento de propinas</MenuItem> 
+                    <MenuItem value='PPP'>Pedido de plano de pagamento</MenuItem> 
+                    <MenuItem value='OT'>Outro</MenuItem> 
+                    </TextField>
+                    <p/>
+                    <Typography align='left' variant='body1' fontFamily='monospace' fontSize={15} paddingBottom={1}>Add comment</Typography>
+                    <TextField 
+                        label="Comment"
+                        value={comment}
+                        onChange={e => setComment(e.target.value)}  
+                        fullWidth
+                        multiline
+                        rows={8}
+                        sx={{backgroundColor:'white'}}>
+                    </TextField>
 
-                </TextField>
-
-            </Stack>
+                    <Stack direction='row' paddingTop='15px' spacing={155}>    
+                         <IconButton
+                            component="label"
+                            sx={{width:'40px',height:'40px', paddingTop:'15px', justifyContent:'left'}}
+                        >
+                            <AttachFileIcon/>
+                            <input
+                                style={{display:"none"}}
+                                type="file"
+                                hidden
+                                onChange={handleFileChange}
+                                name="[licenseFile]"
+                            />
+                        </IconButton>
+                        <Button variant='outlined' type='submit' sx={{ border: 1, borderColor: 'black', backgroundColor: "#81c784", color:'black'}}>Create</Button>
+                    </Stack>
+                    <Stack direction='row' paddingTop='15px'>
+                        {file && `${file.name} - ${file.type}`}
+                    </Stack>
+                </Stack>
+            </form>
+            
         </>
     )
 }
