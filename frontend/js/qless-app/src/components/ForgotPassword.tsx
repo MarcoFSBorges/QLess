edbf38f92051fcdf74023b99482aa162bcbe5c0d
MarcoFSBorges <marcofsborges@gmail.com>
2023-06-10 14:36:05 +0000

    Made ForgotPassword screen more responsive

--- frontend/js/qless-app/src/components/ForgotPassword.tsx
+++ frontend/js/qless-app/src/components/ForgotPassword.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Paper, Stack, Typography, TextField, Button } from '@mui/material';
+import { Paper, Stack, Typography, TextField, Button, Box } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import logo from '../images/QL_1.png'
 
@@ -13,45 +13,37 @@ export function ForgotPassword() {
 
     return (
         <>
-            <Stack direction='row' justifyContent='center' height='300px'>
-                <img src={logo} alt="qless-logo"/>
-            </Stack>
-            
-            <p/>
-            <form onSubmit={handleSubmit}>
-                <Stack direction='row' justifyContent='center'>
-                    <Paper 
-                        elevation={24}
-                        sx={{padding: '8px', width: '50vh'}}>
-                        
-                        <Stack direction='column' >
-                            <Stack direction='row' justifyContent='left'>
-                                <Button variant='outlined' href='/login' startIcon={<ArrowBackIcon/>} disableElevation size='small' sx={{ border: 0, color:'black'}}></Button>  
-                            </Stack> 
-                        </Stack>
+            <Box>
+                <Stack direction='column' justifyContent='center' alignItems='center' spacing={2}>
+                    <img className="qlessLogo" src={logo} alt="qless-logo"/>
 
-                        <Typography align='center' variant='h4' fontFamily='sans-serif' sx={{padding:'4px'}}>Forgot Password</Typography>
-                        <Stack direction='column'>
-                            <TextField 
-                                label='Email' 
-                                variant='outlined' 
-                                value={email} 
-                                onChange={e => setEmail(e.target.value)}
-                                sx={{padding:'4px'}}
-                            />
-                        </Stack>
-                        <p/>
-
-                        <Stack direction='column' >
-                            <Stack direction='row' justifyContent='center'>
+                    <form className="qlessResponsiveForm" onSubmit={handleSubmit}>
+                        <Paper 
+                            elevation={24}
+                            sx={{padding: '24px'}}>
+                            <Box>
+                                <Button variant='outlined' href='/login' startIcon={<ArrowBackIcon/>} disableElevation size='small' sx={{ border: 0, color:'black'}}></Button>
+                            </Box> 
+                            <Stack direction='column' justifyContent='flex-start' alignItems='center' spacing={2}>
+                                
+                                <Typography align='center' variant='h3' fontFamily='sans-serif'>Forgot Password</Typography>
+                                
+                                <TextField 
+                                    fullWidth
+                                    label='Email' 
+                                    variant='outlined' 
+                                    value={email} 
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </Stack>
+                            
+                            <Stack direction='row' justifyContent="center" alignItems='center' spacing={18} sx={{paddingTop:'8px'}}>
                                 <Button variant='outlined' type='submit' sx={{ border: 1, borderColor: 'black',backgroundColor: "#e0e0e0", color:'black'}}>Submit</Button>  
-                            </Stack> 
-                        </Stack>
-                    </Paper>       
-                </Stack> 
-            </form>                         
+                            </Stack>    
+                        </Paper> 
+                    </form>   
+                </Stack>
+            </Box>                  
         </>
-        
     );
-
 }
