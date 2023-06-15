6a9be4308592a4a745e2dd0f389ddfd78e4224cd
a46109 <cravojmls@gmail.com>
2023-06-15 14:50:45 +0100

    storing cookies using react auth kit library

--- frontend/js/qless-app/src/components/Login.tsx
+++ frontend/js/qless-app/src/components/Login.tsx
@@ -1,63 +1,147 @@
-import { useState } from 'react';
-import { Paper, Stack, Typography, TextField, Button, Link, Box } from '@mui/material';
-import logo from '../images/QL_1.png';
+import { useState } from "react";
+import {
+  Paper,
+  Stack,
+  Typography,
+  TextField,
+  Button,
+  Link,
+  Box,
+} from "@mui/material";
+import logo from "../images/QL_1.png";
+import axios from "axios";
+import { useSignIn } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const signIn = useSignIn();
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-    }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
 
-    return (
-        <>
-            <Box>
-                <Stack direction='column' justifyContent='center' alignItems='center' spacing={2}>
-                    <img className="qlessLogo" src={logo} alt="qless-logo"/>
+  const navigate = useNavigate();
 
-                    <form className="qlessResponsiveForm" onSubmit={handleSubmit}>
-                        <Paper 
-                            elevation={24}
-                            sx={{padding: '24px'}}>
-                            <Stack direction='column' justifyContent='flex-start' alignItems='center' spacing={2}>
-                                <Typography align='center' variant='h3' fontFamily='sans-serif'>Login to QLess</Typography>
-                                
-                                <TextField 
-                                    fullWidth
-                                    label='Username' 
-                                    variant='outlined' 
-                                    value={username} 
-                                    onChange={e => setUsername(e.target.value)}
-                                />
+  const logIn = () => {
+    axios
+      .post(
+        "http://localhost:8080/login",
+        { email, password },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        console.log(res.data);
+        signIn({
+          token: res.data,
+          tokenType: "Bearer",
+          expiresIn: 3600,
+          authState: { email: email },
+        });
+        navigate("tickets/abc");
+        alert("Successfully logged in!");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Wrong username or password!");
+      });
+  };
 
-                                <TextField 
-                                    fullWidth
-                                    label='Password' 
-                                    type='password' 
-                                    variant='outlined'
-                                    value={password}
-                                    onChange={e => setPassword(e.target.value)}
-                                />
-                            </Stack>
-                            <Stack direction='column' justifyContent='flex-start' alignItems='flex-start' sx={{paddingTop:'8px'}}>
-                                <Typography align='left' variant='subtitle2'>
-                                    <Link href='/forgot' underline='hover' color='#757575'>Forgot password?</Link>
-                                </Typography>
-                                <Typography align='left' variant='subtitle2'>
-                                    Don't have an account?
-                                </Typography>
-                            </Stack>
-                            
-                            <Stack direction='row' justifyContent="center" alignItems='center' spacing={18} sx={{paddingTop:'8px'}}>
-                                <Button variant='outlined' href='/signup' sx={{border: 1, borderColor: 'black', backgroundColor: "#e0e0e0", color:'black'}}>Sign up</Button>
-                                <Button variant='outlined' type='submit' sx={{ border: 1, borderColor: 'black', backgroundColor: "#e0e0e0", color:'black'}}>Login</Button>  
-                            </Stack>    
-                        </Paper> 
-                    </form>   
-                </Stack>
-            </Box>                  
-        </>
-    );
+  return (
+    <>
+      <Box>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <img className="qlessLogo" src={logo} alt="qless-logo" />
+
+          <form className="qlessResponsiveForm" onSubmit={handleSubmit}>
+            <Paper elevation={24} sx={{ padding: "24px" }}>
+              <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="center"
+                spacing={2}
+              >
+                <Typography align="center" variant="h3" fontFamily="sans-serif">
+                  Login to QLess
+                </Typography>
+
+                <TextField
+                  fullWidth
+                  label="Email"
+                  variant="outlined"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <TextField
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Stack>
+              <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                sx={{ paddingTop: "8px" }}
+              >
+                <Typography align="left" variant="subtitle2">
+                  <Link href="/forgot" underline="hover" color="#757575">
+                    Forgot password?
+                  </Link>
+                </Typography>
+                <Typography align="left" variant="subtitle2">
+                  Don't have an account?
+                </Typography>
+              </Stack>
+
+              <Stack
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={18}
+                sx={{ paddingTop: "8px" }}
+              >
+                <Button
+                  variant="outlined"
+                  href="/signup"
+                  sx={{
+                    border: 1,
+                    borderColor: "black",
+                    backgroundColor: "#e0e0e0",
+                    color: "black",
+                  }}
+                >
+                  Sign up
+                </Button>
+                <Button
+                  onClick={logIn}
+                  variant="outlined"
+                  type="submit"
+                  sx={{
+                    border: 1,
+                    borderColor: "black",
+                    backgroundColor: "#e0e0e0",
+                    color: "black",
+                  }}
+                >
+                  Login
+                </Button>
+              </Stack>
+            </Paper>
+          </form>
+        </Stack>
+      </Box>
+    </>
+  );
 }
--- frontend/js/qless-app/src/components/UserNavBar.tsx
+++ frontend/js/qless-app/src/components/UserNavBar.tsx
@@ -1,131 +1,207 @@
-import { AppBar, Toolbar, IconButton, Stack, Menu, MenuItem, Link, Paper, Typography} from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import Divider from '@mui/material/Divider';
-import logo from '../images/QL_1.png'
-import { useState } from 'react';
-import { Settings } from './Settings';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Stack,
+  Menu,
+  MenuItem,
+  Link,
+  Paper,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import Divider from "@mui/material/Divider";
+import logo from "../images/QL_1.png";
+import { useState } from "react";
+import { Settings } from "./Settings";
+import { useSignOut } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
 
 const hamburguer_menu = [
-    {link:'/createTicket/:qlessId', label:'Create Ticket', isStatus: false}, 
-    {link:'/tickets/:qlessId', label:'My Tickets', isStatus: false}, 
-    {link:'/tickets/:qlessId', label:'Created', isStatus: true},                // /tickets/:qlessId?status=created
-    {link:'/tickets/:qlessId', label:'Ongoing', isStatus: true},               // /tickets/:qlessId?status=ongoing
-    {link:'/tickets/:qlessId', label:'Solved', isStatus: true},                 // /tickets/:qlessId?status=solved
-    {link:'/tickets/:qlessId', label:'Cancelled', isStatus: true},              // /tickets/:qlessId?status=cancelled
-    {link:'/tickets/:qlessId', label:'Archived', isStatus: true},               // /tickets/:qlessId?status=archived
-    {link:'/faq', label:'F.A.Q', isStatus: false}, 
-    {link:'/about', label:'About', isStatus: false}, 
+  { link: "/createTicket/:qlessId", label: "Create Ticket", isStatus: false },
+  { link: "/tickets/:qlessId", label: "My Tickets", isStatus: false },
+  { link: "/tickets/:qlessId", label: "Created", isStatus: true }, // /tickets/:qlessId?status=created
+  { link: "/tickets/:qlessId", label: "Ongoing", isStatus: true }, // /tickets/:qlessId?status=ongoing
+  { link: "/tickets/:qlessId", label: "Solved", isStatus: true }, // /tickets/:qlessId?status=solved
+  { link: "/tickets/:qlessId", label: "Cancelled", isStatus: true }, // /tickets/:qlessId?status=cancelled
+  { link: "/tickets/:qlessId", label: "Archived", isStatus: true }, // /tickets/:qlessId?status=archived
+  { link: "/faq", label: "F.A.Q", isStatus: false },
+  { link: "/about", label: "About", isStatus: false },
 ];
 
 export function UserNavBar() {
+  const [anchorElHamburguer, setAnchorElHamburguer] =
+    useState<null | HTMLElement>(null);
+  const openHamburguer = Boolean(anchorElHamburguer);
 
-    const [anchorElHamburguer, setAnchorElHamburguer] = useState<null | HTMLElement>(null)
-    const openHamburguer = Boolean(anchorElHamburguer)
+  const [anchorElProfile, setAnchorElProfile] = useState<null | HTMLElement>(
+    null
+  );
+  const openProfile = Boolean(anchorElProfile);
 
-    const [anchorElProfile, setAnchorElProfile] = useState<null | HTMLElement>(null)
-    const openProfile = Boolean(anchorElProfile)
+  const [openSettings, setOpenSettings] = useState(false);
 
-    const [openSettings, setOpenSettings] = useState(false);
+  const handleHamburguerClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setAnchorElHamburguer(event.currentTarget);
+  };
 
-    const handleHamburguerClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorElHamburguer(event.currentTarget)
-    }
+  const handleProfileClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElProfile(event.currentTarget);
+  };
 
-    const handleProfileClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorElProfile(event.currentTarget)
-    }
+  const handleHamburguerClose = () => {
+    setAnchorElHamburguer(null);
+  };
 
-    const handleHamburguerClose = () => {
-        setAnchorElHamburguer(null)
-    }
+  const handleProfileClose = () => {
+    setAnchorElProfile(null);
+  };
 
-    const handleProfileClose = () => {
-        setAnchorElProfile(null)
-    }
+  const handleClickOpenSettings = () => {
+    openSettings ? setOpenSettings(false) : setOpenSettings(true);
+  };
 
-    const handleClickOpenSettings = () => {
-        openSettings ? setOpenSettings(false) : setOpenSettings(true)
-    };
-    
+  const signOut = useSignOut();
+  const navigate = useNavigate();
 
-    return (
-        <AppBar position='absolute' elevation={0} sx={{ background: '#f2ebc9'}}>
-            <Toolbar >
-                <IconButton 
-                    id='hamburguer-button' 
-                    onClick={handleHamburguerClick}
-                    size='large' edge='start' 
-                    aria-controls={openHamburguer ? 'hamburguer-menu' : undefined} aria-haspopup='true' aria-expanded={openHamburguer ? 'true' : undefined} aria-label='menu' 
-                >
-                    <MenuIcon/>
-                </IconButton>
-                <Stack direction='row' justifyContent='center' height='150px' sx={{flexGrow: 1}}>
-                    <img src={logo} alt="qless-logo"/>
-                </Stack>
-                <IconButton 
-                    id='profile-button'
-                    onClick={handleProfileClick}
-                    size='large' edge='end' 
-                    aria-controls={openProfile ? 'profile-menu' : undefined} aria-haspopup='true' aria-expanded={openProfile ? 'true' : undefined} aria-label='profile'>
-                    <AccountBoxIcon/>
-                </IconButton>
-                <Menu 
-                    id='hamburguer-menu' anchorEl={anchorElHamburguer} open={openHamburguer} 
-                    MenuListProps={{ 'aria-labelledby' : 'hamburguer-button'}}
-                    onClose={handleHamburguerClose}
-                >
-                    {hamburguer_menu.map((option) => (
-                        <>
-                            <MenuItem key={option.link}> 
-                                {
-                                    option.isStatus ? 
-                                        <Link href={option.link} underline='hover' textAlign="center" color='inherit' fontFamily='monospace' fontWeight={500} paddingLeft={1}>{option.label}</Link> : 
-                                        <Link href={option.link} underline='hover' textAlign="center" color='inherit' fontFamily='monospace' fontWeight={700} >{option.label}</Link>
-                                }
-                            </MenuItem>
-                            {
-                                option.label !== 'About' ? 
-                                    <Divider/> :
-                                    <div/>
-                            }
-                        
-                        </>  
-                    ))}
-                </Menu>
-           
-                <Menu 
-                    id='profile-menu' anchorEl={anchorElProfile} open={openProfile} 
-                    MenuListProps={{ 'aria-labelledby' : 'profile-button'}}
-                    onClose={handleProfileClose}
-                >
-                    <Paper 
-                        elevation={24}
-                        sx={{padding: '16px', width: '45vh'}}
-                    >
-                        <Stack direction='row' justifyContent='center' spacing={1}>
-                            <Typography variant='h5'  fontFamily='monospace' fontSize={25}>First name</Typography>
-                            <Typography variant='h5' fontFamily='monospace' fontSize={25}>Last name</Typography>
-                        </Stack>
-                        <p/>
-                        <Typography align='center' variant='h6' fontFamily='monospace' fontSize={15}>Email</Typography>
-                        <Divider/>
-                        <Stack direction='column' spacing={1}>
-                            <Link onClick={handleClickOpenSettings} underline='hover' textAlign="center" color='inherit' fontFamily='monospace' fontSize={20} paddingTop={1}>Settings</Link>
-                            
-                            {
-                                openSettings ? <Settings/> : null
-                            }
-                            
-                            <Link underline='hover' textAlign="center" color='inherit' fontFamily='monospace' fontSize={20}>Logout</Link>
-                        </Stack>
-                    </Paper>
-                </Menu>
-            </Toolbar>
-        </AppBar>
+  const logout = () => {
+    signOut();
+    navigate("/login");
+  };
 
+  return (
+    <AppBar position="absolute" elevation={0} sx={{ background: "#f2ebc9" }}>
+      <Toolbar>
+        <IconButton
+          id="hamburguer-button"
+          onClick={handleHamburguerClick}
+          size="large"
+          edge="start"
+          aria-controls={openHamburguer ? "hamburguer-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={openHamburguer ? "true" : undefined}
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          height="150px"
+          sx={{ flexGrow: 1 }}
+        >
+          <img src={logo} alt="qless-logo" />
+        </Stack>
+        <IconButton
+          id="profile-button"
+          onClick={handleProfileClick}
+          size="large"
+          edge="end"
+          aria-controls={openProfile ? "profile-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={openProfile ? "true" : undefined}
+          aria-label="profile"
+        >
+          <AccountBoxIcon />
+        </IconButton>
+        <Menu
+          id="hamburguer-menu"
+          anchorEl={anchorElHamburguer}
+          open={openHamburguer}
+          MenuListProps={{ "aria-labelledby": "hamburguer-button" }}
+          onClose={handleHamburguerClose}
+        >
+          {hamburguer_menu.map((option) => (
+            <>
+              <MenuItem key={option.link}>
+                {option.isStatus ? (
+                  <Link
+                    href={option.link}
+                    underline="hover"
+                    textAlign="center"
+                    color="inherit"
+                    fontFamily="monospace"
+                    fontWeight={500}
+                    paddingLeft={1}
+                  >
+                    {option.label}
+                  </Link>
+                ) : (
+                  <Link
+                    href={option.link}
+                    underline="hover"
+                    textAlign="center"
+                    color="inherit"
+                    fontFamily="monospace"
+                    fontWeight={700}
+                  >
+                    {option.label}
+                  </Link>
+                )}
+              </MenuItem>
+              {option.label !== "About" ? <Divider /> : <div />}
+            </>
+          ))}
+        </Menu>
 
-        
-           
-    )
+        <Menu
+          id="profile-menu"
+          anchorEl={anchorElProfile}
+          open={openProfile}
+          MenuListProps={{ "aria-labelledby": "profile-button" }}
+          onClose={handleProfileClose}
+        >
+          <Paper elevation={24} sx={{ padding: "16px", width: "45vh" }}>
+            <Stack direction="row" justifyContent="center" spacing={1}>
+              <Typography variant="h5" fontFamily="monospace" fontSize={25}>
+                First name
+              </Typography>
+              <Typography variant="h5" fontFamily="monospace" fontSize={25}>
+                Last name
+              </Typography>
+            </Stack>
+            <p />
+            <Typography
+              align="center"
+              variant="h6"
+              fontFamily="monospace"
+              fontSize={15}
+            >
+              Email
+            </Typography>
+            <Divider />
+            <Stack direction="column" spacing={1}>
+              <Link
+                onClick={handleClickOpenSettings}
+                underline="hover"
+                textAlign="center"
+                color="inherit"
+                fontFamily="monospace"
+                fontSize={20}
+                paddingTop={1}
+              >
+                Settings
+              </Link>
+
+              {openSettings ? <Settings /> : null}
+
+              <Link
+                onClick={logout}
+                underline="hover"
+                textAlign="center"
+                color="inherit"
+                fontFamily="monospace"
+                fontSize={20}
+              >
+                Logout
+              </Link>
+            </Stack>
+          </Paper>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
 }
